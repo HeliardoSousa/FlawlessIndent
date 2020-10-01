9b294abb6ef52b0c48636497b7c90266e4d55580
AlimpioBrito <48600229+AlimpioBrito@users.noreply.github.com>
2020-10-01 20:41:22 -0300

    Add files via upload
    atualização do projeto para vuejs

---
+++ src/components/ListProdutos.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="listProdutos">
+    <br>
+    <br>
+    <br>
+    <br>
+     <table id="produtos">
+        <tr>
+            <td>
+                <div id="1" class="card">
+                    <img class="card-img-top" src="../assets/placa-mae-gigabyte-ga-a320m-h-amd-am4-matx-ddr4-rev-1-1.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Placa-Mãe Gigabyte</h5>
+                        <p class="card-text">Soquete AM4: Suporte...</p>
+                        <a href="produto.html" class="btn btn-primary">COMPRAR</a>
+                    </div>
+                </div>
+            </td>
+            <td>
+                <div id="2" class="card">
+                    <img class="card-img-top" src="../assets/hyperx.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Memória RAM DDR4</h5>
+                        <p class="card-text">Formato: UDIMM /Pinos...</p>
+                        <a href="#" class="btn btn-primary">COMPRAR</a>
+                    </div>
+                </div>
+            </td>
+            <td>
+                <div id="3" class="card">
+                    <img class="card-img-top" src="../assets/processador-intel-core-i7-10gen.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Intel Core i7</h5>
+                        <p class="card-text">Segmento vertical: Desktop...</p>
+                        <a href="#" class="btn btn-primary">COMPRAR</a>
+                    </div>
+                  </div>
+            </td>
+            <td>
+                <div id="4" class="card">
+                    <img class="card-img-top" src="../assets/HD SSD Samsung EVO.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">>HD SSD Samsung EVO</h5>
+                        <p class="card-text">Soquete AM4: Suporte...</p>
+                        <a href="#" class="btn btn-primary">COMPRAR</a>
+                    </div>
+                  </div>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <div id="5" class="card">
+                    <img class="card-img-top" src="../assets/processador-amd-ryzen-3-3200g.webp" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">AMD Ryzen-3-3200g</h5>
+                        <p class="card-text">Soquete AM4: Suporte...</p>
+                        <a href="#" class="btn btn-primary">COMPRAR</a>
+                    </div>
+                  </div>
+            </td>
+            <td>
+                <div id="6" class="card">
+                    <img class="card-img-top" src="../assets/placa-de-video-gigabyte-amd-radeon-rx-5500-xt.jpg"
+                        alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">AMD rx 5500xt</h5>
+                        <p class="card-text">Formato: UDIMM /Pinos...</p>
+                        <a href="#" class="btn btn-primary">COMPRAR</a>
+                    </div>
+                </div>
+            </td>
+            <td>
+                <div id="7" class="card">
+                    <img class="card-img-top" src="../assets/gamemax-m908-01.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">gamemax m908-01</h5>
+                        <p class="card-text">Soquete AM4: Suporte...</p>
+                        <a href="#" class="btn btn-primary">COMPRAR</a>
+                    </div>
+                  </div>
+            </td>
+            <td>
+                <div id="8" class="card">
+                    <img class="card-img-top" src="../assets/memoria ram crucial ballistix.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Memória RAM DDR4</h5>
+                        <p class="card-text">Soquete AM4: Suporte...</p>
+                        <a href="#" class="btn btn-primary">COMPRAR</a>
+                    </div>
+                  </div>
+            </td>
+        </tr>
+    </table>
+    <br><br>
+    <div align='center'>
+        <button class="btn btn-primary" onclick="listarEntidade('item')"> Ver Mais </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+#produtos{
+    margin-left: auto;
+    margin-right: auto
+}
+
+.card-img-top{
+    width: 286px;
+    height: 180px;
+}
+
+.card:hover{
+    transform: scale(1.1);
+    position: static;
+}
+
+#produtos tr{
+    margin-left: 7px;
+}
+
+.card-text{
+    text-align: justify;
+}
+
+.card{
+    margin-left: auto;
+    margin-right: auto;
+    width: 18rem;
+}
+.card-body{
+    text-align: center; 
+}
+
+.imgProduct{
+    width: 286px;
+    height: 180px;
+    object-fit: scale-down;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="Login">
+        <div class="main-container">
+        <h1>Tela de login - SHATEC</h1>
+        <div class="campos">
+            <form id="form">
+                <div class="full-box form-group">
+                    <label for="email">Email:</label>
+                    <input type="email" class="form-control" id="email" placeholder="Digite seu email" validade-obrigatorio validade-email>
+                </div>
+                <div class="full-box form-group">
+                    <label for="password">Senha:</label>
+                    <input type="password" class="form-control" id="password" placeholder="Digite sua senha" validade-tamanho="8" validade-obrigatorio >
+                </div>
+                <div class="buttons">
+                    <button type="submit" id="btn-submit" class="btn btn-primary">Entrar</button>
+                    <a href="cadastro.html"><button type="button" class="btn btn-primary">Cadastre-se</button></a>
+                </div>
+            </form>
+        </div>
+    </div>
+    <p class="erro-validacao template"></p>
+  </div>
+</template>
+
+<style scoped>
+.main-container{
+    width: 500px;
+    margin-left: auto;
+    margin-right: auto;
+    background-color: white;
+    border-radius: 10px;
+    padding: 25px;
+}
+
+.main-container h1{
+    text-align: center;
+    margin-bottom: 25px;
+    font-size: 1.6rem;
+}
+
+.erro-validacao{
+    color: #ff1a1a;
+    position: absolute;
+    top: 67px;
+    font-size: .8rem;
+}
+
+.template{
+    display: none;
+}
+
+.buttons{
+    text-align: center;
+    margin-top: 75px;
+}
+
+.btn{
+    width: 120px;
+}
+
+.full-box{
+    position: relative;
+}
+</style>
